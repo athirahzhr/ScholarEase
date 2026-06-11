ebc9880bdebbce52d43d5c5b8e709b0e97a12d2d
athirah zahari <athirahzahari13@gmail.com>
2026-06-11 22:30:40 +0800

    layout fix

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -19,20 +19,7 @@
                     </div>
                 </div>
                 <div class="card-body p-0">
-                    @if(session('success'))
-                        <div class="alert alert-success alert-dismissible fade show m-3" role="alert">
-                            <i class="fas fa-check-circle me-2"></i>
-                            {{ session('success') }}
-                            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-                        </div>
-                    @endif
-                    @if(session('error'))
-                        <div class="alert alert-danger alert-dismissible fade show m-3" role="alert">
-                            <i class="fas fa-exclamation-circle me-2"></i>
-                            {{ session('error') }}
-                            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-                        </div>
-                    @endif
+                   
                     
                     <div class="table-responsive">
                         <table class="table table-hover mb-0" id="usersTable">
