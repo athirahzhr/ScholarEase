8fd70e57d20bce8bd0cacd460c3700dbcc3966a2
athirah zahari <athirahzahari13@gmail.com>
2026-06-28 02:07:06 +0800

    design

--- resources/views/scholarship/finder.blade.php
+++ resources/views/scholarship/finder.blade.php
@@ -718,7 +718,7 @@
 
 @push('scripts')
 <!-- Load SweetAlert2 with fallback -->
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" onerror="handleSwalError()"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
 <script>
