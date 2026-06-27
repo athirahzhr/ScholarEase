1e03fe2866ba280a89d5cadd5afa75383d399e8d
athirah zahari <athirahzahari13@gmail.com>
2026-06-28 02:22:35 +0800

    design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -417,7 +417,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     
     <!-- SweetAlert2 - PASTIKAN INI ADA! -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
     
     <!-- AOS Animation -->
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
